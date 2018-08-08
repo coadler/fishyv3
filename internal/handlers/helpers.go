afd85394a40c9793406dd3d62d0031079897bc98
Colin <cadler@coder.com>
2018-08-08 11:58:57 -0500

    Use liftDB in inTxn errors

--- internal/handlers/helpers.go
+++ internal/handlers/helpers.go
@@ -4,17 +4,13 @@ import (
 	"context"
 	"database/sql"
 
-	"github.com/pkg/errors"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
 	"github.com/coadler/fishyv3/internal/models"
 )
 
 func inTxn(ctx context.Context, db *sql.DB, fn func(txn models.XODB) error) error {
 	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
-		return status.Error(codes.Internal, errors.Wrap(err, "failed to start transaction").Error())
+		return liftDB(err, "failed to start transaction")
 	}
 	defer tx.Rollback()
 
@@ -24,7 +20,7 @@ func inTxn(ctx context.Context, db *sql.DB, fn func(txn models.XODB) error) erro
 	}
 
 	if err := tx.Commit(); err != nil {
-		return status.Error(codes.Internal, errors.Wrap(err, "failed to commit transaction").Error())
+		return liftDB(err, "failed to commit transaction")
 	}
 	return nil
 }
